5c2b76874d656da5f54ddc9d6f0823fbd43f9871
Joshua DSouza <joshuand1990@gmial.com>
2018-12-04 13:24:06 +0400

    return types

--- src/ExcelSpreadSheetReader.php
+++ src/ExcelSpreadSheetReader.php
@@ -65,6 +65,8 @@ abstract class ExcelSpreadSheetReader extends BaseExcelSpreadSheet
         $this->spreadsheet = IOFactory::createReaderForFile($filePath);
         $this->spreadsheet->setReadDataOnly(true);
         $this->loadWorkBook($filePath);
+
+        return $this;
     }
 
     /**
